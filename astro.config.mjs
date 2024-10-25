9ec3f208dbc7cc3ba670e82ccc4bc0cd1e40ea07
Martin Ristovski <martin.s.ristovski@gmail.com>
2024-10-25 10:23:34 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://weltedwiki.com',
+	base: 'martin.shoes/',
 	integrations: [
 		starlight({
 			title: 'WeltedWiki.com',
